a216b0c8d00e66fd7b21a67b3c769f7d926faa77
José Postiga <jpostiga@infraspeak.com>
2020-02-02 11:01:22 +0000

    fix/style): fix pagination links not breaking lines in mobile

--- source/articles.blade.php
+++ source/articles.blade.php
@@ -18,19 +18,19 @@ pagination:
     @endforeach
 
     @if ($pagination->pages->count() > 1)
-        <nav class="flex text-base my-8">
+        <nav class="flex flex-wrap text-base mt-8">
             @if ($previous = $pagination->previous)
-                <a href="{{ $previous }}" title="Previous Page" class="bg-grey-lighter hover:bg-grey-light rounded mr-3 px-5 py-3">&LeftArrow;</a>
+                <a href="{{ $previous }}" title="Previous Page" class="bg-grey-lighter hover:bg-grey-light rounded mr-3 mb-3 px-5 py-3">&LeftArrow;</a>
             @endif
 
             @foreach ($pagination->pages as $pageNumber => $path)
-                <a href="{{ $path }}" title="Go to Page {{ $pageNumber }}" class="bg-grey-lighter hover:bg-grey-light text-blue-darker rounded mr-3 px-5 py-3 {{ $pagination->currentPage == $pageNumber ? 'text-blue-dark' : '' }}">
+                <a href="{{ $path }}" title="Go to Page {{ $pageNumber }}" class="bg-grey-lighter hover:bg-grey-light text-blue-darker rounded mr-3 mb-3 px-5 py-3 {{ $pagination->currentPage == $pageNumber ? 'text-blue-dark' : '' }}">
                     {{ $pageNumber }}
                 </a>
             @endforeach
 
             @if ($next = $pagination->next)
-                <a href="{{ $next }}" title="Next Page" class="bg-grey-lighter hover:bg-grey-light rounded mr-3 px-5 py-3">&RightArrow;</a>
+                <a href="{{ $next }}" title="Next Page" class="bg-grey-lighter hover:bg-grey-light rounded mr-3 mb-3 px-5 py-3">&RightArrow;</a>
             @endif
         </nav>
     @endif
